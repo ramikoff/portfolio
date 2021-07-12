cea27099270b189c469eab49615a13e7ec45a3b1
Ramil Novruzov <75280600+ramikoff@users.noreply.github.com>
2021-07-12 14:45:47 +0200

    Add initial Portfolio files

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Sticky Header
+/*beim scrollen des Dokumentes nach unten , wird ab einer gewissen Scroll-Höhe über das script.js dem Header zu der vorhanden Klasse .main_h eine weitere Klasse .sticky hinzugefügt/ergänzt */
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
